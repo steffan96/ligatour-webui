412a2dd8088f34cbfc7f272b1e0cc9cdfac3be2d
steffan96 <stefanmilicic@yahoo.com>
2026-01-21 19:13:05 +0100

    static mvp

--- src/components/leagues/CreateComponent.tsx
+++ src/components/leagues/CreateComponent.tsx
@@ -8,14 +8,20 @@ export default function CreateComponent({type}: {type: 'leagues' | 'tournaments'
 			<div className='flex flex-row justify-between items-start mt-8'>
 				<CreateComponentHeader
 					title={type === 'leagues' ? 'Leagues' : 'Tournaments'}
-					description={`Create and manage your ${type}s`}
+					description={`Create and manage your ${type}`}
 				/>
 				<Button text={`+ Create ${type === 'leagues' ? 'League' : 'Tournament'}`} className='mt-4 ml-4 rounded-md' onClick={() => {
 					// TODO: Implement create functionality
 				}} />
 			</div>
-			<div className=''>
-
+			<div className='ml-4 border-2 border-dotted border-black p-4'>
+				<div className='flex flex-col justify-center items-center'>
+					<h1 className='text-lg font-semibold mb-4'>{`No ${type} yet`}</h1>
+					<p>{`Create your first ${type === 'leagues' ? 'league' : 'tournament'} to get started.`}</p>
+					<Button text={`+ Create ${type === 'leagues' ? 'League' : 'Tournament'}`} className='mt-4 ml-4 rounded-md' onClick={() => {
+						// TODO: Implement create functionality
+					}} />
+				</div>
 			</div>
 		</>
 	);
