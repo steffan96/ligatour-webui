9910dc682f18d7884fb012eb841523482da9dd26
steffan96 <stefanmilicic@yahoo.com>
2026-02-12 18:23:26 +0100

    sidebarcompetitions

--- src/components/home/LandingLayout.tsx
+++ src/components/home/LandingLayout.tsx
@@ -5,11 +5,11 @@ export default function LandingLayout() {
 	return (
 		<div className='flex w-full p-8'>
 			<div className='w-[85%] ml-auto'>
-				<div className='ml-2'>
-					<h1 className='text-4xl p-2'>Competition Manager</h1>
-					<h4 className='text-lg p-2'>Create and manage your leagues and tournaments</h4>
-					<CreateComponent />
-				</div>
+			<h1 className="max-w-4xl mx-auto text-4xl md:text-5xl tracking-tight mb-6 mt-16">
+				Laying the Foundation for{' '}
+				<span className="text-green-900">League Play</span>
+			</h1>
+			<CreateComponent />
 			</div>
 		</div>
 	);
--- src/components/home/RootLayout.tsx
+++ src/components/home/RootLayout.tsx
@@ -3,6 +3,7 @@ import { Outlet } from 'react-router-dom';
 import Header from '../shared/Header';
 import { InfoMessageCard } from '../shared/InfoMessageCard';
 import { useToastStore } from '../../api/stores/useToastStore';
+import SidebarCompetitions from '../competitions/SidebarCompetitions';
 
 export default function RootLayout() {
 	const { toast, hideToast } = useToastStore();
@@ -13,6 +14,7 @@ export default function RootLayout() {
 			)}
 			<div className='flex flex-col h-full w-full'>
 				<Header />
+				<SidebarCompetitions />
 				<main className='flex-1 bg-gray-300 overflow-auto'>
 					<Outlet />
 				</main>
