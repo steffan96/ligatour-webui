d1c16d2f80de73937663f55c2edbf809f360e54c
steffan96 <stefanmilicic@yahoo.com>
2026-06-02 19:25:52 +0200

    small cleanup

--- src/api/interfaces/competitions.ts
+++ src/api/interfaces/competitions.ts
@@ -1,14 +1,12 @@
 enum CompetitionType {
 	ROUND_ROBIN = "round_robin",
 	KNOCKOUT = "knockout",
-	GROUP_STAGE = "group_stage",
 	SWISS = "swiss",
 }
 
 const CompetitionTypeDisplay: Record<CompetitionType, string> = {
 	[CompetitionType.ROUND_ROBIN]: "Round Robin",
 	[CompetitionType.KNOCKOUT]: "Knockout",
-	[CompetitionType.GROUP_STAGE]: "Group Stage",
 	[CompetitionType.SWISS]: "Swiss System",
 };
 
